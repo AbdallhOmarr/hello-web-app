0746e8f035a3ccb5bafb316c6d7b4f7a4ecefb4b
Abdallh Omarr <AbdallhOmarr@gmail.com>
2023-02-14 22:31:43 +0200

    start file

--- default.nix
+++ default.nix
@@ -1,62 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-with pkgs;
-
-let
-  src = fetchFromGitHub {
-    owner = "AbdallhOmarr";
-    repo = "hello-web-app";
-    rev = "latest-commit-sha";
-    sha256 = "sha256-hash-of-latest-commit";
-  };
-
-in
-python38.mkDerivation {
-  pname = "simple-web-app";
-  version = "1.0";
-  src = src;
-  buildInputs = [
-    uwsgi
-    postgresql
-    nginx
-    git
-    openssl
-    zlib
-  ];
-  preBuild = ''
-    . ${pkgs.python38}/bin/activate
-    pip install -r ${./simple_web_app/requirements.txt}
-  '';
-  postInstall = ''
-    mkdir -p $out/nginx/conf
-    echo '
-    worker_processes 1;
-
-    events {
-        worker_connections 1024;
-    }
-
-    http {
-        include mime.types;
-        default_type application/octet-stream;
-        sendfile on;
-        keepalive_timeout 65;
-        gzip on;
-
-        server {
-            listen 8080;
-
-            location / {
-                uwsgi_pass unix:///run/uwsgi.sock;
-                include uwsgi_params;
-            }
-        }
-    }' > $out/nginx/conf/nginx.conf
-  '';
-  meta = with pkgs.stdenv.lib; {
-    description = "A simple Django web app";
-    maintainers = [ "AbdallhOmar AbdallhOmarr@gmail.com" ];
-    license = licenses.mit;
-    platforms = [ "x86_64-linux" ];
-  };
+pkgs.python3.withPackages (ps: with ps; [
+  django
+  gunicorn
+]) // {
+  buildInputs = [ pkgs.gcc ];
 }
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+. /opt/venv/bin/activate
+cd simple_web_app
+gunicorn simple_web_app.wsgi:application --bind 0.0.0.0:$PORT
